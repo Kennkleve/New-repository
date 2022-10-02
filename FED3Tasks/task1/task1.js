b6f5de53570eaaa57636f5cbeac173813fd42b01
Kenneth Klever <Kennkleve@gmail.com>
2022-10-02 17:27:35 +0200

    tasks upload

---
+++ FED3Tasks/task1/task1.js
@@ -0,0 +1,9 @@
+//Sets the background colour of all classA elements to green
+//changes the font size of all paragraphs to 20px
+//adds 10px of left padding to the fourth paragraph.
+
+$(function () {
+  $('.classA').css('background-color', 'green');
+  $('p').css('font-size', '20px');
+  $('#para4').css('padding-left', '10px');
+});
