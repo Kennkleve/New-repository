3b6b3698438fcc4fa61bf190ed07377123ee77ca
Kenneth Klever <Kennkleve@gmail.com>
2022-10-07 13:06:58 +0200

    update

--- api_example.js
+++ api_example.js
@@ -1,5 +1,5 @@
 $.ajax({
-  url: 'https://github.com/Kennkleve/New-repository',
+  url: 'https://api.github.com/repos/Kennkleve/New-repository',
   success: function (response) {
     console.log(response);
   },
